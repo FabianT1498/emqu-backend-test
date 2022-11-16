a1743874a6a27a9774ce0c5dcfcff66abf3b5814
Fabian Trillo <fabiantrillo1498@gmail.com>
2022-11-16 17:41:04 -0400

    feat: se ha agregado el seeder de los usuarios

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,8 +5,10 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
+
 class AuthServiceProvider extends ServiceProvider
 {
+    
     /**
      * The model to policy mappings for the application.
      *
@@ -26,5 +28,6 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         //
+        
     }
 }
